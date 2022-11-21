8fa4d1fba4a1af8c80a1d9b974d27d0d67ddd2d5
Carla Willmes <willmes.carla@gmail.com>
2022-11-21 16:35:03 +0900

    reorganized components

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
-function Header() {
-  return (
-      <header>
-          <nav className="navbar">
-              <img src="./react-logo.png" width="40px" />
-              <ul className="nav-items">
-                  <li>Pricing</li>
-                  <li>About</li>
-                  <li>Contact</li>
-              </ul>
-          </nav>
-      </header>
-  )
+function App() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
 }
 
-function Page() {
-    <div>
-        <Header />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-}
-
-// ReactDOM.render(page, document.getElementById("root"))
+// ReactDOM.render(<App />, document.getElementById("root"))
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<Page />)
+root.render(<App />)
